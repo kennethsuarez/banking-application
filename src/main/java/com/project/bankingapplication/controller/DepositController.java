422e5c2f25c34ac029f76502ed382a0cfebcf152
Kenneth Liwanag Suarez/Web Solutions Group /SRPH/Engineer/Samsung Electronics <k.suarez@samsung.com>
2022-12-01 15:11:36 +0800

    add hateoas implementation; create separate controller for deposit and withdraw actions

--- src/main/java/com/project/bankingapplication/controller/DepositController.java
+++ src/main/java/com/project/bankingapplication/controller/DepositController.java
@@ -1,7 +1,6 @@
 package com.project.bankingapplication.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.net.URI;
 import java.util.List;
@@ -9,6 +8,7 @@ import java.util.Optional;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,67 +23,50 @@ import com.project.bankingapplication.exception.AccountNotFoundException;
 import com.project.bankingapplication.service.AccountRepository;
 import com.project.bankingapplication.service.TransactionRepository;
 
-
-
 @RestController
-public class AccountController {
+public class DepositController {
 	
 	private AccountRepository repository;
-	
 	
 	private TransactionRepository transactionRepository;
 	
 	
-	public AccountController(AccountRepository repository, TransactionRepository transactionRepository) {
+	public DepositController(AccountRepository repository, TransactionRepository transactionRepository) {
 		this.repository = repository;
 		this.transactionRepository = transactionRepository;
 	}
 	
 	
-	@GetMapping("/api/accounts")
-	public List<Account> retrieveAllAccounts() {
-		return repository.findAll();
-	}
-	
-	
-	@GetMapping("/api/accounts/{id}")
-	public EntityModel<Optional<Account>> retrieveUser(@PathVariable int id) {
+	@PostMapping("/api/accounts/{id}/deposit")
+	public ResponseEntity<Object> depositMoney(@PathVariable int id, @RequestBody Transaction transaction) {
 		Optional<Account> account = repository.findById(id);
+		Account accountEntity = null;
 		
-		if (account.isEmpty()) 
+		if (account.isEmpty()) {
+			
 			throw new AccountNotFoundException("id:"+id);
+			
+		} else {
+			
+			accountEntity = account.get();
+			
+		}
 		
-		EntityModel<Optional<Account>> entityModel = EntityModel.of(account);
+		transaction.setAccount(accountEntity);
 		
-		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllTransactions(id));
-		entityModel.add(link.withRel("transactions"));
+			
+		accountEntity.setBalance(accountEntity.getBalance() + transaction.getAmount());
+
 		
-		return entityModel;
-	}
-	
-	
-	@PostMapping("/api/accounts")
-	public ResponseEntity<Object> createUser(@RequestBody Account account) {
-		Account savedAccount = repository.save(account);
+		transaction.setTransactionType("deposit");
+		Transaction savedTransaction = transactionRepository.save(transaction);
+		repository.save(accountEntity);
 		
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}")
-				.buildAndExpand(savedAccount.getId())
+				.buildAndExpand(savedTransaction.getId())
 				.toUri();
 		
 		return ResponseEntity.created(location).build();
 	}
-	
-	@GetMapping("/api/accounts/{id}/transactions")
-	public List<Transaction> retrieveAllTransactions(@PathVariable int id) {
-		Optional<Account> account = repository.findById(id);
-		
-		if (account.isEmpty())
-			throw new AccountNotFoundException("id:"+id);
-		
-		return account.get().getTransactions();
-	}
-	
-	
-
 }
